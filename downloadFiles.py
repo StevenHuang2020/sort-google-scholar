b203ff6e0237988ed8683eb324f5f77c437719b7
StevenHuang2020 <61686583+StevenHuang2020@users.noreply.github.com>
2020-03-14 17:13:24 +1300

    Add files via upload

---
+++ downloadFiles.py
@@ -0,0 +1,60 @@
+#python3 Steven
+#14/03/2020 download a batch of files use wget 
+import os
+import wget      #pip instal wget
+import argparse
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""
+#usgae:
+#python .\downloadFiles.py --ft ./examples/tt.txt --dst ./doc/
+"""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+FILESTXT = ''   #files list file
+DSTPATH = '.'   #default dest folder
+
+def get_command_line_args():
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Arguments')
+    parser.add_argument('--ft', type=str, help='Txt file contain files list you want to download')
+    parser.add_argument('--dst', type=str, help='Downlaod destination folder')
+    
+    # Parse and read arguments and assign them to variables if exists
+    args, _ = parser.parse_known_args()
+
+    filesText = FILESTXT
+    if args.ft:
+        filesText = args.ft
+
+    dstPath = DSTPATH
+    if args.dst:
+        dstPath = args.dst
+
+    return filesText, dstPath
+
+def downloadFile(url,dstPath):
+    #print('Beginning file download with wget module')
+    fileName = filename = url[url.rfind("/")+1:]
+    if dstPath == DSTPATH:
+        dst = fileName
+    else:
+        dst = dstPath + "\\" + fileName
+
+    print('url=',url)
+    print('filename=',filename)
+    print('dst=',dst)
+    wget.download(url, out=dst)
+
+def main():
+    FILESTXT, DSTPATH = get_command_line_args()
+    print(FILESTXT, DSTPATH)
+    if not os.path.exists(DSTPATH):
+        os.mkdir(DSTPATH)
+
+    with open(FILESTXT) as fp:
+        for line in fp:
+            url = line.rstrip()
+            downloadFile(url,DSTPATH)
+    
+if __name__=='__main__':
+    main()
+    
